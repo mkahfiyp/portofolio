5048efbc75d3ee093b0905b08eb0bf0d9da5183e
mkahfiyp <mkahfi467@gmail.com>
2025-07-03 11:41:03 +0700

    hapus import script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Script from "next/script";
+// import Script from "next/script";
 import ThemeProvider from "@/components/theme-provider";
 
 // const poppins = Poppins({
